c61b5457a037c202f6425b57b7b165527fad4644
Axel Rivière <axel_riviere@ens.univ-artois.fr>
2024-10-24 15:22:53 +0200

    #43-incident-execution Suppression du chemin predefini de la compilation C et C++

--- app/src/main/java/lens/judge/b5/compiler/CCompilationStrategy.java
+++ app/src/main/java/lens/judge/b5/compiler/CCompilationStrategy.java
@@ -12,13 +12,15 @@ public class CCompilationStrategy extends AbstractCompilationStrategy {
 
     @Override
     public String getBinaryName(String sourceFile) {
-        return sourceFile.replace(".c", "");
+        String [] parts = sourceFile.split("/");
+        String fileNameWithExtension = parts[parts.length - 1];
+        return fileNameWithExtension.replace(".c", "");
     }
 
     @Override
     protected String getCompileCommand(String sourceFile, String binaryName) {
         System.out.println("Compiling C code : gcc -x c -Wall -O2 -static -pipe -lm -o app/bin/" + binaryName + " " + sourceFile);
-        return "gcc -x c -Wall -O2 -static -pipe -lm -o app/bin/" + binaryName + " app/src/test/resources/" + sourceFile;
+        return "gcc -x c -Wall -O2 -static -pipe -lm -o app/bin/" + binaryName + " " + sourceFile;
     }
 
     public void getPath(String binaryName) {
