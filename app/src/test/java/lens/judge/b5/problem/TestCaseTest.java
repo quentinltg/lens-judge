596e781bd1868ef2bbe1240e21b5a1f64dc5a738
Axel Rivière <axel_riviere@ens.univ-artois.fr>
2024-10-25 09:41:35 +0200

    #48-incident-tests-problem Update des tests du package problem

--- app/src/test/java/lens/judge/b5/problem/TestCaseTest.java
+++ app/src/test/java/lens/judge/b5/problem/TestCaseTest.java
@@ -1,33 +1,24 @@
 package lens.judge.b5.problem;
 
 import org.junit.Test;
+import java.io.File;
 import static org.junit.Assert.*;
 
 public class TestCaseTest {
 
     @Test
-    public void testGetInput() {
-        TestCase testCase = new TestCase("input1", "output1");
-        assertEquals("input1", testCase.getInput());
-    }
-
-    @Test
-    public void testSetInput() {
-        TestCase testCase = new TestCase("input1", "output1");
-        testCase.setInput("newInput");
-        assertEquals("newInput", testCase.getInput());
-    }
-
-    @Test
     public void testGetOutput() {
-        TestCase testCase = new TestCase("input1", "output1");
-        assertEquals("output1", testCase.getOutput());
+        File outputFile = new File("output1");
+        File expectedFile = new File("expected1");
+        TestCase testCase = new TestCase(outputFile, expectedFile);
+        assertEquals(outputFile, testCase.getOutput());
     }
 
     @Test
-    public void testSetOutput() {
-        TestCase testCase = new TestCase("input1", "output1");
-        testCase.setOutput("newOutput");
-        assertEquals("newOutput", testCase.getOutput());
+    public void testGetExpected() {
+        File outputFile = new File("output1");
+        File expectedFile = new File("expected1");
+        TestCase testCase = new TestCase(outputFile, expectedFile);
+        assertEquals(expectedFile, testCase.getExpected());
     }
 }
