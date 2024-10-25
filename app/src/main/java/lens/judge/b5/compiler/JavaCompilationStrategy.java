5e2ce80bd771406d9377b38361465a643ef465c6
quentin_tripognez <quentin_tripognez@ens.univ-artois.fr>
2024-10-25 14:56:36 +0200

    #42 - Faire de la doc ! Documentation des classes du package compiler. Erreurs restantes SonarLint => -System.out.println() -Exception

--- app/src/main/java/lens/judge/b5/compiler/JavaCompilationStrategy.java
+++ app/src/main/java/lens/judge/b5/compiler/JavaCompilationStrategy.java
@@ -1,17 +1,40 @@
 package lens.judge.b5.compiler;
 
+/**
+ * Compilation strategy for Java source files.
+ * This class provides methods to check compatibility, generate compile commands, and verify the existence of compiled binaries.
+ */
 public class JavaCompilationStrategy extends AbstractCompilationStrategy {
 
+    /**
+     * Checks if the given source file is compatible with this compilation strategy.
+     *
+     * @param sourceFile the path to the source file
+     * @return true if the file is a Java source file, false otherwise
+     */
     @Override
     public boolean isCompatible(String sourceFile) {
         return sourceFile.endsWith(".java");
     }
 
+    /**
+     * Generates the binary name from the given source file.
+     *
+     * @param sourceFile the path to the source file
+     * @return the name of the binary file to be produced
+     */
     @Override
     public String getBinaryName(String sourceFile) {
         return sourceFile.replace(".java", ".class");
     }
 
+    /**
+     * Constructs the compile command for the given source file and binary name.
+     *
+     * @param sourceFile the path to the source file
+     * @param binaryName the name of the binary to be produced
+     * @return the compile command as a string
+     */
     @Override
     public String getCompileCommand(String sourceFile, String binaryName) {
         // System.out.println("Compiling Java code : javac -d app/bin/ " + sourceFile);
--- app/src/main/java/lens/judge/b5/compiler/PythonCompilationStrategy.java
+++ app/src/main/java/lens/judge/b5/compiler/PythonCompilationStrategy.java
@@ -1,17 +1,42 @@
 package lens.judge.b5.compiler;
 
+/**
+ * Compilation strategy for Python source files.
+ * This class provides methods to check compatibility and generate compile commands for Python files.
+ */
 public class PythonCompilationStrategy extends AbstractCompilationStrategy {
 
+    /**
+     * Checks if the given source file is compatible with this compilation strategy.
+     *
+     * @param sourceFile the path to the source file
+     * @return true if the file is a Python source file, false otherwise
+     */
     @Override
     public boolean isCompatible(String sourceFile) {
         return sourceFile.endsWith(".py");
     }
 
+    /**
+     * Generates the binary name from the given source file.
+     * For Python files, this method returns null as Python does not produce a binary file.
+     *
+     * @param sourceFile the path to the source file
+     * @return null as Python does not produce a binary file
+     */
     @Override
     public String getBinaryName(String sourceFile) {
         return null;
     }
 
+    /**
+     * Constructs the compile command for the given source file.
+     * This method generates the command to compile Python source files.
+     *
+     * @param sourceFile the path to the source file
+     * @param binaryName the name of the binary to be produced (not used for Python)
+     * @return the compile command as a string
+     */
     @Override
     protected String getCompileCommand(String sourceFile, String binaryName) {
         System.out.println("Compiling Python code : python3 -m py_compile " + sourceFile);
