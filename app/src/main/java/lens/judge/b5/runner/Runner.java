a9e5b2df661ae7cc32b18f6b86632cda3b341ae1
Bourhim Bylel <bylel_bourhim@ens.univ-artois.fr>
2024-10-25 16:21:10 +0200

    Ajout commentaires de Runner

--- app/src/main/java/lens/judge/b5/runner/Runner.java
+++ app/src/main/java/lens/judge/b5/runner/Runner.java
@@ -2,7 +2,6 @@ package lens.judge.b5.runner;
 
 import lens.judge.b5.compiler.ICompilationStrategy;
 import lens.judge.b5.execution.IExecutionStrategy;
-import lens.judge.b5.verifier.StrictComparer;
 import lens.judge.b5.verifier.Verifier;
 
 import java.io.BufferedWriter;
@@ -10,17 +9,35 @@ import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 
+/**
+ * The Runner class is responsible for compiling, executing, and verifying the output of a source file.
+ */
 public class Runner {
     private IExecutionStrategy executionStrategy;
     private ICompilationStrategy compilationStrategy;
     private String sourceFile;
 
+    /**
+     * Constructs a Runner with the specified execution and compilation strategies and source file.
+     *
+     * @param executionStrategy the strategy to execute the compiled program
+     * @param compilationStrategy the strategy to compile the source file
+     * @param sourceFile the path to the source file to be compiled and executed
+     */
     public Runner(IExecutionStrategy executionStrategy, ICompilationStrategy compilationStrategy, String sourceFile) {
         this.executionStrategy = executionStrategy;
         this.compilationStrategy = compilationStrategy;
         this.sourceFile = sourceFile;
     }
 
+    /**
+     * Compiles and executes the source file with the given input file, and verifies the output against the expected output file.
+     *
+     * @param inputFile the input file to be used during execution
+     * @param expectedOuputFile the file containing the expected output
+     * @param comparer the verifier to compare the actual output with the expected output
+     * @return true if the output matches the expected output, false otherwise
+     */
     public boolean run(File inputFile, File expectedOuputFile, Verifier comparer) {
         File outputFile = new File("app/src/main/resources/output.ans");
 
@@ -57,7 +74,6 @@ public class Runner {
             // 3. Verify the output
             System.out.print("\nOutput: " + output);
             boolean result = comparer.verify(outputFile, expectedOuputFile);
-            // System.out.println("Verification result: " + result);
             return result;
 
         } catch (Exception e) {
