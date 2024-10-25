958c54e612f79ffbbf29361b8355be546376cb98
quentin_tripognez <quentin_tripognez@ens.univ-artois.fr>
2024-10-25 16:57:50 +0200

    #42 - Faire de la doc ! Documentation du package execution

--- app/src/main/java/lens/judge/b5/execution/CExecutionStrategy.java
+++ app/src/main/java/lens/judge/b5/execution/CExecutionStrategy.java
@@ -8,27 +8,44 @@ import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.OutputStream;
 
+/**
+ * Execution strategy for C/C++ binaries.
+ */
 public class CExecutionStrategy implements IExecutionStrategy {
 
     private String binaryName;
     private ProcessAdapter process;
 
+    /**
+     * Constructs a new CExecutionStrategy with the specified binary name.
+     *
+     * @param binaryName the name of the binary to execute
+     */
     public CExecutionStrategy(String binaryName) {
         this.binaryName = binaryName;
     }
 
+    /**
+     * Returns the process associated with this execution strategy.
+     *
+     * @return the process associated with this execution strategy
+     */
     @Override
     public IProcess getProcess() {
         return process;
     }
 
+    /**
+     * Executes the binary with the provided input file.
+     *
+     * @param inputFile the input file to be passed to the binary
+     * @throws Exception if an error occurs during execution
+     */
     @Override
     public void execute(File inputFile) throws Exception {
-        // System.out.println("Executing C/C++ code : app/bin/" + binaryName);
         process = new ProcessAdapter("app/bin/" + binaryName);
         process.start();
 
-        // Écrire l'entrée dans le processus
         try (FileInputStream fis = new FileInputStream(inputFile);
              OutputStream processInput = process.getOutputStream()) {
             byte[] buffer = new byte[1024];
@@ -36,7 +53,7 @@ public class CExecutionStrategy implements IExecutionStrategy {
             while ((bytesRead = fis.read(buffer)) != -1) {
                 processInput.write(buffer, 0, bytesRead);
             }
-            processInput.flush();  // S'assurer que toutes les données sont bien transmises
+            processInput.flush();
         } catch (IOException e) {
             System.out.println("Error writing to process input: " + e.getMessage());
             throw e;
